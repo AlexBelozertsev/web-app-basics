85035d6a877ed34dd1d1496233540272ce7ca95c
AlexBelozertsev <kashim983@gmail.com>
2020-12-07 13:24:39 +0200

    добавил комменты по настройке

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # web-app-basics
 
+- [Alexander Repeta - пошаговая инструкция написания web-app](https://www.youtube.com/watch?v=p4XGoF0sFJM&feature=youtu.be&ab_channel=GoIT)
 - [Alex Slepchenkov - JAMstack и с чем его едят](https://youtu.be/b1ZnAmIXXAQ)
 - [A Step By Step Guide To Using Handlebars With Your Node js App](https://medium.com/@waelyasmina/a-guide-into-using-handlebars-with-your-express-js-application-22b944443b65)
 - [expressjs.com](https://expressjs.com/)
-- [Deploying with Git](https://devcenter.heroku.com/articles/git#prerequisites-install-git-and-the-heroku-cli)
+- # [Deploying with Git](https://devcenter.heroku.com/articles/git#prerequisites-install-git-and-the-heroku-cli)
+- ======================================================================================================== -
+- в package.json:
+- "scripts":
+- { "start": "node app.js",
+- "dev": "nodemon app.js"
+- //nodemon вместо node для исключения необходимости перезапуска 'npm start' при
+  каждом изменении исходных файлов. достигается 'npm i nodemon D' (Go live for
+  BackEnd)
+- },
--- app.js
+++ app.js
@@ -1,42 +1,51 @@
-const express = require('express');
-const exhbs = require('express-handlebars');
-const products = require('./products.json');
+const express = require('express'); //подключение пакета express
+const exhbs = require('express-handlebars'); //подключение библиотеки handlebars
+const products = require('./products.json'); //подключение database
 
 const PORT = process.env.PORT || 4444;
 
-const app = express();
+const app = express(); //вызов функции express() возвращает объект app методами которого создается web-server
 
-app.use(express.static('public'));
-app.set('view engine', 'hbs');
+app.use(express.static('public')); //регистрация настройки раздачи стат.ресурсов(img,css,js) прослойка, пропускает запрос через адрес ('public'), что позволяет appserver видеть(обрабатывать)ресурсы
+app.set('view engine', 'hbs'); //регистрация движка express на handlebars (по-умолчанию стоит pug - https://pugjs.org/api/getting-started.html)
 app.engine(
   'hbs',
   exhbs({
     extname: 'hbs',
-  }),
+  }), // настройка конфигурации view engine (устанавливает расширение hbs)
 );
-
+//запрос по маршруту url localhost:4444/ вызывает регистрацию слушателя входящего соединения
+// req - request(запрос); console.log(req.url) ==> "/"
+// res - response(ответ); res.send({key: value}) ==> in browser {"key":"value"} typeOf - json;
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home'); //рендерит содержимое (home) из handlebar (views/home.hbs) в {layouts: 'main'} вместо {{{body}}}; и переводит в HTML
 });
-
+//запрос по маршруту url localhost:4444/about вызывает регистрацию слушателя входящего соединения
+// req.url ==> "/about"
+// res.send('<h1>Hi. This is /about</h1>') ==> in browser: h1 string
 app.get('/about', (req, res) => {
-  res.render('about', { cssFileName: 'about', pageTitle: 'О нас' });
+  res.render('about', { cssFileName: 'about', pageTitle: 'О нас' }); //передача css в виде параметра cssFileName; title в виде параметра pageTitle в main.hbs
 });
-
+//запрос по маршруту url localhost:4444/products вызывает регистрацию слушателя входящего соединения
 app.get('/products', (req, res) => {
   res.render('products', {
     products,
     cssFileName: 'products',
     pageTitle: 'Наши продукты',
-  });
+  }); //передача объекта настроек {{#each products}}, css (stylesheets/products.css) в products
 });
 
 app.get('/product/:productId', (req, res) => {
+  // : в пути "/:productId" указатель динамич.параметров
   console.log(req.params);
-
+  // сравнение по id для перехода на страницу товара
   const product = products.find(p => p.id === req.params.productId);
 
-  res.render('product', { product });
+  res.render('product', {
+    product,
+    cssFileName: 'products',
+    pageTitle: 'Информация о товаре',
+  });
 });
 
 app.listen(PORT, () => {
